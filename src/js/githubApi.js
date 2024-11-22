99f0ff5d923db3175dfe4679b1c5f73a88d414ab
Muhamad Haisyam Khairizmi <81277845+Haisyam@users.noreply.github.com>
2024-11-22 12:36:50 +0700

    Update githubApi.js

--- src/js/githubApi.js
+++ src/js/githubApi.js
@@ -1,8 +1,7 @@
 const username = "Haisyam";
-const token =
-  "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"; // Replace with your token
 const repoContainer = document.getElementById("repos");
 
+// Fungsi untuk mengambil semua halaman repository
 async function fetchAllRepositories() {
   let page = 1;
   let repos = [];
@@ -11,19 +10,15 @@ async function fetchAllRepositories() {
   while (fetchMore) {
     try {
       const response = await fetch(
-        `https://api.github.com/users/${username}/repos?page=${page}&per_page=100&sort=updated`,
-        {
-          headers: {
-            Authorization: `token ${token}`,
-          },
-        }
+        `https://api.github.com/users/${username}/repos?page=${page}&per_page=100&sort=updated`
       );
       const data = await response.json();
 
+      // Jika data kosong, maka semua repositori sudah diambil
       if (data.length === 0) {
         fetchMore = false;
       } else {
-        repos = repos.concat(data);
+        repos = repos.concat(data); // Gabungkan data halaman ini ke daftar repositori
         page++;
       }
     } catch (error) {
@@ -35,12 +30,16 @@ async function fetchAllRepositories() {
   return repos;
 }
 
+// Fungsi untuk menampilkan repositori di HTML
 function displayRepositories(repos) {
-  repoContainer.innerHTML = "";
+  // Sortir repositori berdasarkan waktu pembaruan terbaru
+  repos.sort((a, b) => new Date(b.updated_at) - new Date(a.updated_at));
+
+  repoContainer.innerHTML = ""; // Kosongkan kontainer sebelum menampilkan data baru
   repos.forEach((repo) => {
     const repoElement = document.createElement("div");
     repoElement.className =
-      "bg-white p-4 rounded-lg shadow-md border-l-4 border-blue-950 pb-14";
+      "bg-white p-4 rounded-lg shadow-md border-l-4 border-blue-950 mb-4";
     repoElement.innerHTML = `
       <h2 class="text-lg font-bold text-gray-800">
         <a href="${repo.html_url}" target="_blank">${repo.name}</a>
@@ -49,15 +48,17 @@ function displayRepositories(repos) {
       <p class="text-sm text-gray-500 mt-2">
         Updated at: ${new Date(repo.updated_at).toLocaleString()}
       </p>
-    `;
+        `;
     repoContainer.appendChild(repoElement);
   });
 }
 
+// Fungsi utama untuk mengambil dan menampilkan repositori
 async function loadRepositories() {
   const repos = await fetchAllRepositories();
   displayRepositories(repos);
 }
 
+// Memanggil loadRepositories() secara berkala untuk update real-time
 loadRepositories();
-setInterval(loadRepositories, 60000);
+setInterval(loadRepositories, 60000); // Update setiap 60 detik
