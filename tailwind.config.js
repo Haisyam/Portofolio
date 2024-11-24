d933f0c00bee1e3606784e1cd5b238aaf555fc5a
Haisyam <bersholawat2019@gmail.com>
2024-11-24 21:17:21 +0700

    add: tiktok download

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["index.html"],
+  content: ["index.html", "page/**/*{html}", "page/info/**/*{html}"],
   theme: {
     container: {
       center: true,
